093e8c703a5d6b1a50f94259eedeb5b1e77a6bad
Amâncio Santos <amanciosantos10@gmail.com>
2023-09-09 01:20:44 -0300

    inicio

---
+++ Thread Excel/U_Principal.pas
@@ -0,0 +1,28 @@
+unit U_Principal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.Imaging.jpeg, Vcl.ExtCtrls,
+  Vcl.StdCtrls, Vcl.Buttons;
+
+type
+  TFrm_Principal = class(TForm)
+    Image1: TImage;
+    Image2: TImage;
+    Button1: TButton;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Frm_Principal: TFrm_Principal;
+
+implementation
+
+{$R *.dfm}
+
+end.
